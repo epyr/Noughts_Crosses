8c37203bb2ce99d3beaebaca54dda5e5b097efe6
Łukasz Jurczuk <atfe@interia.pl>
2018-09-26 08:58:48 +0200

    Cleaned up graphical version #2

--- NoughtsCrosses.js
+++ NoughtsCrosses.js
@@ -15,16 +15,12 @@ function setupGame() {
     player1Won = false;
     player2Won = false;
     draw = false;
-
     imgBlank = new Image();
     imgBlank.src = "BlankTile.JPG";
-
     imgCross = new Image();
     imgCross.src = "Cross.JPG";
-
     imgNought = new Image();
     imgNought.src = "Nought.JPG";
-    
     gameWon = false;
 
     var p1mark = prompt("Kółko czy kRzyżyk? K/R");
@@ -42,26 +38,20 @@ function setupGame() {
 
         player1.mark = "X";
         player2.mark = "O";
-
         player1.imgSrc = imgCross.src;
         player2.imgSrc = imgNought.src;
     }
 
     player1.winValue = 3;
     player2.winValue = 30;
-
     player1.val = 1;
     player2.val = 10;
-
     currentPlayer = player1;
-
     playingField = new Array(3);
-
     for (var i = 0; i < 3; i++) {
 
         playingField[i] = new Array(3);
     }
-
 
     for (var i = 0; i < 3; i++) {
 
@@ -76,29 +66,25 @@ function setupGame() {
 function printField() {
 
     var printout = "";
-
     for (var i = 0; i < 3; i++) {
 
         for (var j = 0; j < 3; j++) {
 
             printout = printout + playingField[i][j] + "   ";
         }
-
+        
         document.write("<br>");
         document.write(printout);
         document.write("<br>");
         printout = "";
 
     }
-
-
 }
 
 function handleDraw() {
     
     alert("Remis");
 }
-
 
 function handleVictory(player) {
     
@@ -127,7 +113,6 @@ function makeMove(row, column, DOMelement) {
         if (movesLeft === 0) {
             handleDraw();
         }
-
     }
 
     if (currentPlayer === player1) {
@@ -145,7 +130,6 @@ function makeMove(row, column, DOMelement) {
 function checkVictory() {
 
     var sum = 0;
-
     for (var r = 0; r < 3; r++) {
 
         for (var c = 0; c < 3; c++) {
@@ -161,7 +145,6 @@ function checkVictory() {
         else{
             sum = 0;
         }
-
     }
   
         sum = 0;
@@ -176,13 +159,11 @@ function checkVictory() {
             if (sum === currentPlayer.winValue) {
             
             handleVictory(currentPlayer);
-            return;
-                      
+            return;      
             }
             
             else{
-            
-            sum = 0;
+                sum = 0;
             }       
         }
         
@@ -195,16 +176,13 @@ function checkVictory() {
         
         if (sum === currentPlayer.winValue){
             
-            handleVictory(currentPlayer);
-           
+            handleVictory(currentPlayer);  
             return;
         }
-        
         else{
             
             sum = 0;
         }  
-        
         sum = 0;
         sum = playingField[0][2] + playingField[1][1] + playingField[2][0];
         
@@ -213,7 +191,6 @@ function checkVictory() {
             handleVictory(currentPlayer);
             return;
         }
-        
         return;
  }
 
@@ -222,7 +199,6 @@ function onTileClick(element) {
     var id = element.getAttribute("id");
     var row = id.substring(3, 4);
     var col = id.substring(4);
-
     row = Number(row);
     col = Number(col);
     makeMove(row, col, element);
